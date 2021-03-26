377e5121a77076e6072c5ce24b0c81f6af8f5bf7
benji <benjaminoperhall@mymail.champlain.edu>
2021-03-26 09:20:50 -0400

    Added visuals for TitleSceen and combined level layouts

---
+++ SnailBoarder/Assets/Scenes/DesignScenes/DesignScripts/ParkMaster.cs
@@ -0,0 +1,98 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using SuperiorArrays;
+using UnityEngine.SceneManagement;
+public class ParkMaster : MonoBehaviour
+{
+    public static GameObject seedInfo;
+    public GameObject highway;
+    private GameObject[][] parkLayoutHighway;
+
+
+    private GameObject timerObject;
+
+
+    public string nextScene;
+
+
+
+    public float levelLength;
+
+
+    private void Awake()
+    {
+       
+
+    }
+
+    // Start is called before the first frame update
+    void Start()
+    {
+
+        SetLevelParameter(0);
+
+        timerObject = GameObject.Find("UI_Main");
+        AssignSlots(highway);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        CheckTimer();
+    }
+
+
+    private void CheckTimer()
+    {
+
+        if (Time.time - timerObject.GetComponent<SkateTimer>().skateStart > levelLength)
+        {
+
+            CallScene();
+
+
+        }
+    }
+
+    private void CallScene()
+    {
+        SceneManager.LoadScene(nextScene);
+
+    }
+
+    public void AssignSlots(GameObject section)
+    {
+        GameObject[] tempArray = new GameObject[0];
+        for (int i = 0; i < section.transform.childCount; i++)
+        {
+
+
+          //  for (int j = 0; j < length; j++)
+          //  {
+            //    Sray.AppendGameObjectArray(tempArray, section.transform.GetChild(i).gameObject);
+            //}
+
+            
+
+            
+
+
+        }
+
+    }
+
+
+
+
+
+    public void SetLevelParameter(int type)
+    {
+
+
+
+    }
+
+
+
+}
---
+++ SnailBoarder/Assets/Scenes/DesignScenes/DesignScripts/SuperiorArrays.cs
@@ -0,0 +1,60 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+
+namespace SuperiorArrays {
+
+
+    class Sray
+    {
+
+
+
+        public static string[] AppendStringArray(string[] sArray, string addition)
+        {
+            string[] tempArray = new string[sArray.Length + 1];
+            for (int i = 0; i < sArray.Length; i++)
+            {
+                tempArray[i] = sArray[i];
+            }
+            tempArray[tempArray.Length - 1] = addition;
+            return tempArray;
+        }
+
+
+        public static float[] AppendFloatArray(float[] fArray, float addition)
+        {
+            float[] tempArray = new float[fArray.Length + 1];
+            for (int i = 0; i < fArray.Length; i++)
+            {
+                tempArray[i] = fArray[i];
+            }
+            tempArray[tempArray.Length - 1] = addition;
+            return tempArray;
+        }
+
+        public static GameObject[] AppendGameObjectArray(GameObject[] gArray, GameObject addition)
+        {
+            GameObject[] tempArray = new GameObject[gArray.Length + 1];
+            for (int i = 0; i < gArray.Length; i++)
+            {
+                tempArray[i] = gArray[i];
+            }
+            tempArray[tempArray.Length - 1] = addition;
+            return tempArray;
+        }
+
+    }
+
+
+
+
+        
+
+
+
+
+
+}
