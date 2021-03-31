764c1ed821e3e1dbccd8b14dd404be2e0cb02d79
Wesley Elmer <wesley.elmer@mymail.champlain.edu>
2021-03-31 14:42:24 -0400

    Dialogue Changes
    - Implemented Dialogue Blocks for Cutscene_1
    - Renamed CutsceneOne -> TestDialogue
    - Adjustments made to DialogueBlock (scriptable object) to improve usability

--- SnailBoarder/Assets/Scenes/DesignScenes/DesignScripts/DialogueBlock.cs
+++ SnailBoarder/Assets/Scenes/DesignScenes/DesignScripts/DialogueBlock.cs
@@ -13,10 +13,13 @@ public class DialogueBlock : ScriptableObject
 
     public string[] japaneseDialogueLines;
 
-    public float textSpeed;
+    [Tooltip("Amount of time inbetween each letter being rendered (in seconds.)")]
+    [Range(0,1)]
+    public float textSpeed = 0.1f;
 
     public Animation emotion;
 
+    [Tooltip("The Dialogue master has camera positions childed to it. This int represents the child that the camera will share the position of.")]
     public int cameraSlot;
     
 
