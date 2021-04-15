8fac5b3d51c806ba6acc2d8e1a84da3076ae4c0a
Egor Fesenko <fesenkoegor239@gmail.com>
2021-04-16 02:26:13 +0300

    Finishing merge

--- SnailBoarder/Assets/Scripts/SceneLoader.cs
+++ SnailBoarder/Assets/Scripts/SceneLoader.cs
@@ -30,17 +30,17 @@ public class SceneLoader : MonoBehaviour
         }
         else if (sceneName == "judge")
         {
-            // Load the judge scene
-            //SceneManager.LoadScene(judge);
-            //currentScene = judge;
+            //Load the judge scene
+            SceneManager.LoadScene(judge);
+            currentScene = judge;
 
         }
         else if (sceneName == "dialog")
         {
-            // Signal that we've finished the previous level (may need to add loading test here)
-            //GameManager.instance.OnFinishedLevel();
-            //SceneManager.LoadScene(dialog);
-            //currentScene = dialog;
+            //Signal that we've finished the previous level (may need to add loading test here)
+            GameManager.instance.OnFinishedLevel();
+            SceneManager.LoadScene(dialog);
+            currentScene = dialog;
         }
         else if (sceneName == "title")
         {
