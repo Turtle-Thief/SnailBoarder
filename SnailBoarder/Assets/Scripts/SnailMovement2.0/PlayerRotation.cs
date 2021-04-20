4359e233bccba35846ea4130fcc4a8715fb913b6
benji <benjaminoperhall@mymail.champlain.edu>
2021-04-20 19:05:19 -0400

    Added z axis rotation, modified snail collider and rays, itterated on rotation

--- SnailBoarder/Assets/Scripts/SnailMovement2.0/PlayerRotation.cs
+++ SnailBoarder/Assets/Scripts/SnailMovement2.0/PlayerRotation.cs
@@ -5,7 +5,7 @@ using UnityEngine.InputSystem;
 public class PlayerRotation : MonoBehaviour
 {
 
-
+    //X Axis Rays
     public GameObject pointRayFront;
     public GameObject pointRayBack;
 
@@ -14,13 +14,22 @@ public class PlayerRotation : MonoBehaviour
     public GameObject visualPointBack;
 
 
+    //Y Axis Rays
+
+    public GameObject pointRayLeft;
+    public GameObject pointRayRight;
+
+
+    public GameObject visualPointLeft;
+    public GameObject visualPointRight;
+
 
     public GameObject snailBody;
 
 
     public float rotSpeed;
 
-    private float yRot;
+    public float yRot;
 
 
     // Start is called before the first frame update
@@ -32,22 +41,39 @@ public class PlayerRotation : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-     //   print("Front " + CastRayDown(pointRayFront.transform));
-     //   print("Back " + CastRayDown(pointRayBack.transform));
+      //  print("Front " + CastRayDown(pointRayFront.transform));
+      //  print("Back " + CastRayDown(pointRayBack.transform));
 
-
+        //Converts Positions into usable Vector 2s
+        //X Axis
         Vector2 frontPoint = new Vector2(CastRayDown(pointRayFront.transform).z, CastRayDown(pointRayFront.transform).y);
         Vector2 backPoint = new Vector2(CastRayDown(pointRayBack.transform).z, CastRayDown(pointRayBack.transform).y);
 
-        //print("Angle" + GetAngle(frontPoint, backPoint));
+        //Y Axis
+        Vector2 leftPoint = new Vector2(CastRayDown(pointRayLeft.transform).x, CastRayDown(pointRayLeft.transform).y);
+        Vector2 rightPoint = new Vector2(CastRayDown(pointRayRight.transform).x, CastRayDown(pointRayRight.transform).y);
 
+
+
+
+        //Debug Visual of Where Rays hit
         visualPointFront.transform.position = CastRayDown(pointRayFront.transform);
         visualPointBack.transform.position = CastRayDown(pointRayBack.transform);
+        visualPointLeft.transform.position = CastRayDown(pointRayLeft.transform);
+        visualPointRight.transform.position = CastRayDown(pointRayRight.transform);
 
+     //   print("Left " + leftPoint);
+      //  print("Right " + rightPoint);
+      //  print("Front " + frontPoint);
+    //    print("Back " + backPoint);
+     //   print(GetAngle(leftPoint, rightPoint));
 
+        //Applying Angles to snail model
+        ApplyAngle(GetAngle(frontPoint, backPoint), 0, GetAngle(leftPoint, rightPoint));
 
+        //Applying Y rotation to overall snail
+        ApplyTurn(yRot);
 
-        ApplyAngle(GetAngle(frontPoint, backPoint), yRot, 0);
     }
 
 
@@ -66,18 +92,36 @@ public class PlayerRotation : MonoBehaviour
     }
 
 
+    public void ApplyTurn(float rotation)
+    {
+        Vector3 originRot = transform.eulerAngles;
+        transform.eulerAngles = new Vector3(originRot.x, rotation, originRot.z);
+
+
+    }
+
+
+
+
     //Applys all gathered angle data
     public void ApplyAngle(float xAngle,float yAngle, float zAngle)
     {
-        snailBody.transform.eulerAngles = new Vector3(xAngle *-90f , yAngle, zAngle);
+        snailBody.transform.localRotation = Quaternion.Euler(xAngle *-90f , yAngle, zAngle *90f);
     }
 
 
     //Math to get slope of 2 points, if theres already a function dont tell me
     public float GetAngle(Vector2 pointA, Vector2 pointB)
     {
+
         float rise = pointB.y - pointA.y;
         float run = pointB.x - pointA.x;
+        if (rise == run)
+        {
+            print(rise);
+            print(run);
+            return 0;
+        }
         float slope = rise / run;
 
         slope = (Mathf.Atan(slope)) / (Mathf.PI / 2);
@@ -90,7 +134,7 @@ public class PlayerRotation : MonoBehaviour
     public Vector3 CastRayDown(Transform point)
     {
         RaycastHit hit;
-        Physics.Raycast(point.position, Vector3.down, out hit, 100f);
+        Physics.Raycast(point.position, Vector3.down, out hit, 200f);
         return hit.point;
     }
 
