856f91667ea369838b7e4b2e79f655c3d5440b09
Chelsey Sanford <csanford43@gmail.com>
2021-04-19 22:00:52 -0400

    there is some impr(m)ovement
    i would not touch this branch right now if you are not chelsey, i am making good progress but in the interest of not getting burnt out/frustrated am pausing for now and wrote some notes to pick up on tomorrow!

--- SnailBoarder/Assets/Scripts/SnailMovement2.0/PlayerVelocity.cs
+++ SnailBoarder/Assets/Scripts/SnailMovement2.0/PlayerVelocity.cs
@@ -5,16 +5,34 @@ using UnityEngine.InputSystem;
 
 public class PlayerVelocity : MonoBehaviour
 {
-    public GroundCheck groundCheck;
-    public Rigidbody   rigidbody;
+    public float acceleration = 50.0f,
+                     braking = 10.0f,
+                     friction = 5.0f,
+                     maxForwardSpeed = 100.0f,
+                     maxReverseSpeed = -30.0f,
+                     gravity = 0.5f,
+                     rotationSpeed = 100.0f,
+                     currentSpeed;
 
-    public float acceleration = 50.0f, braking = 10.0f, currentSpeed, friction = 5.0f, maxForwardSpeed = 100.0f, maxReverseSpeed = -30.0f;
+    [HideInInspector]
+    public GroundCheck groundCheck;
+    [HideInInspector]
+    public Rigidbody rigidbody;
+
+    public Vector2 inputRotation;
+
+    [HideInInspector] public Quaternion PhysicsRotation;
+    [HideInInspector] public Quaternion VelocityRotation;
+    [HideInInspector] public Quaternion InputRotation;
+    [HideInInspector] public Quaternion ComputedRotation;
+
+    public GameObject frontWheels, backWheels;
 
     // Start is called before the first frame update
     void Start()
     {
         groundCheck = gameObject.GetComponent<GroundCheck>();
-        rigidbody   = gameObject.GetComponent<Rigidbody>();
+        rigidbody = gameObject.GetComponent<Rigidbody>();
     }
 
     private void Update()
@@ -29,12 +47,128 @@ public class PlayerVelocity : MonoBehaviour
     {
         if (!GameManager.instance.gameIsPaused)
         {
-            if (groundCheck.isGrounded)
-            {
-                VelocityUpdate();
-            }
+            CheckPhysics();
+            SkaterMove(inputRotation);
+            Debug.DrawRay(transform.position, transform.up, Color.cyan);
         }
     }
+
+    // player is accelerating
+    void OnMoveForward()
+    {
+        currentSpeed += acceleration;
+        //needs to add force
+        Vector3 Direction = transform.forward.normalized * currentSpeed;
+        rigidbody.AddForce(Direction, ForceMode.VelocityChange);
+    }
+
+    // player is braking
+    void OnBrake()
+    {
+        currentSpeed -= braking;
+        //needs to add force
+        Vector3 Direction = InputRotation * transform.forward.normalized * currentSpeed;
+        rigidbody.AddForce(-Direction, ForceMode.VelocityChange);
+    }
+
+    void OnTurn(InputValue rot)
+    {
+        inputRotation = rot.Get<Vector2>();
+    }
+
+    void CheckPhysics()
+    {
+        if (!groundCheck.isGrounded)
+        {
+            rigidbody.velocity += Vector3.down * gravity;
+        }
+    }
+
+    void SkaterMove(Vector2 inputs)
+    {
+
+        PhysicsRotation = !groundCheck.isGrounded ? Quaternion.identity : GetPhysicsRotation(); // Rotation according to ground normal 
+        VelocityRotation = GetVelocityRot();
+        InputRotation = Quaternion.identity;
+        ComputedRotation = Quaternion.identity;
+
+
+        if (inputs.magnitude > 0.1f)
+        {
+            Vector3 adapted_direction = ProcessInput(inputs);
+            Vector3 planar_direction = transform.forward;
+            planar_direction.y = 0;
+            InputRotation = Quaternion.FromToRotation(planar_direction, adapted_direction);
+
+            if (groundCheck.isGrounded)
+            {
+                Vector3 Direction = InputRotation * transform.forward.normalized * currentSpeed;
+                //rigidbody.AddForce(Direction, ForceMode.VelocityChange);
+                /* NEXT STEPS:
+                    - Make this rotate the snail
+                    - Add a buffer to get velocity rot
+                    - profit and/or cry             */
+            }
+        }
+
+        ComputedRotation = PhysicsRotation * VelocityRotation * transform.rotation;
+        transform.rotation = Quaternion.Lerp(transform.rotation, ComputedRotation, rotationSpeed * Time.deltaTime);
+    }
+
+    Quaternion GetVelocityRot()
+    {
+        Vector3 vel = rigidbody.velocity;
+        if (vel.magnitude > 0.2f)
+        {
+            vel.y = 0;
+            Vector3 dir = transform.forward;
+            dir.y = 0;
+            Quaternion vel_rot = Quaternion.FromToRotation(dir.normalized, vel.normalized);
+            return vel_rot;
+        }
+        else
+            return Quaternion.identity;
+    }
+
+    Vector3 ProcessInput(Vector2 d)
+    {
+        inputRotation = Vector2.zero;
+        Vector3 player = transform.position;
+        player.y = 0;
+
+        Vector3 player_right = Quaternion.AngleAxis(90, Vector3.up) * player;
+
+        Vector3 direction = player * d.y + player_right * d.x;
+        return direction.normalized;
+    }
+
+    Quaternion GetPhysicsRotation()
+    {
+        Vector3 target_vec = Vector3.up;
+        Ray ray = new Ray(transform.position, Vector3.down);
+        Ray rayFront = new Ray(frontWheels.transform.position, Vector3.down);
+        Ray rayBack = new Ray(backWheels.transform.position, Vector3.down);
+        RaycastHit hit, hitFront, hitBack;
+
+        if (Physics.Raycast(rayFront, out hitFront, 1.05f) && Physics.Raycast(rayBack, out hitBack, 1.05f))
+        {
+            target_vec = hitFront.normal + hitBack.normal;
+            //target_vec.y = 0;
+            // instead I need to get the normals at each wheel
+
+            if ((hitFront.normal - hitBack.normal).magnitude <= 0.5f)
+            {
+                if (Physics.Raycast(ray, out hit, 1.05f))
+                    target_vec = hit.normal;
+                else
+                    target_vec = Vector3.up;
+            }
+        }
+
+        return Quaternion.FromToRotation(transform.up, target_vec);
+    }
+
+
 
     // calculate velocity
     void VelocityUpdate()
@@ -48,18 +182,6 @@ public class PlayerVelocity : MonoBehaviour
         if (changeInSpeed.magnitude >= 0.2f)
             rigidbody.velocity += changeInSpeed;
         Mathf.Clamp(rigidbody.velocity.magnitude, -maxReverseSpeed, maxForwardSpeed);
-    }
-
-    // player is accelerating
-    void OnMoveForward()
-    {
-        currentSpeed += acceleration;
-    }
-
-    // player is braking
-    void OnBrake()
-    {
-        currentSpeed -= braking;
     }
 
     // player friction
